4c121c56e9ec4829d15f62ee32a7b84104d3c6af
iceee-xxix <kenzo.lay1936@gmail.com>
2025-05-28 12:22:56 +0700

    update

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0" />
-    <title>Login Shop</title>
+    <title>DEMO - HR</title>
     <meta name="description" content="" />
     <link rel="icon" type="image/x-icon" href="{{asset('assets/img/favicon/favicon.ico')}}" />
     <link rel="preconnect" href="https://fonts.googleapis.com" />
